22b594c3c904b48794db5fe9380e71ef3367c6a9
Jetross <jetrossneri07@gmail.com>
2025-04-16 21:45:33 +0800

    added background

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,21 @@
 <template>
-  <h1 class="h1main">Homepage</h1>
+  <v-card
+    class="d-flex justify-center align-center background-image"
+    style="height: 100vh; width: 100vw"
+  >
+    <h1 class="h1main">Homepage</h1>
+  </v-card>
 </template>
 <script setup></script>
 <style scoped>
 .h1main {
   font-family: "Gugi", cursive;
+  color: white;
+}
+.background-image {
+  background-image: url("../assets/main-bg.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 </style>
