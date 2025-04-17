26101b7c8c2de8519ba52b02c1fc49abd37533e6
Jetross <jetrossneri07@gmail.com>
2025-04-17 14:14:20 +0800

    added navbar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,13 @@
     class="d-flex justify-center align-center background-image"
     style="height: 100vh; width: 100vw"
   >
+    <v-app-bar rounded class="navbar">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>Application Bar</v-app-bar-title>
+    </v-app-bar>
     <h1 class="h1main">Homepage</h1>
   </v-card>
 </template>
@@ -14,6 +21,13 @@
 }
 .background-image {
   background-image: url("../assets/main-bg.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+.navbar {
+  background-image: url("../assets/main-bg.jpg");
+  color: white;
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
