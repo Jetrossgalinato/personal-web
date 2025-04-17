f9c6b9e41482fd09b4eefc5e7f766ca320301c56
Jetross <jetrossneri07@gmail.com>
2025-04-17 14:30:59 +0800

    added navigation items

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,29 @@
 <template>
   <v-card
-    class="d-flex justify-center align-center background-image"
+    class="d-flex flex-column background-image"
     style="height: 100vh; width: 100vw"
   >
     <v-app-bar rounded class="navbar">
-      <template v-slot:prepend>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      </template>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-app-bar-title>Application Bar</v-app-bar-title>
+      <div
+        class="d-flex position-absolute left-0 right-0 justify-center"
+        style="gap: 30px"
+      >
+        <span>About</span>
+        <span>Projects</span>
+        <span>Contact</span>
+      </div>
     </v-app-bar>
-    <h1 class="h1main">Homepage</h1>
   </v-card>
 </template>
+
 <script setup></script>
 <style scoped>
 .h1main {
   font-family: "Gugi", cursive;
   color: white;
+  text-align: left;
 }
 .background-image {
   background-image: url("../assets/main-bg.jpg");
